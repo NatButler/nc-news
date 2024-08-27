135e7899b9d693d89e62fbfc2200f5993933b85a
Nathaniel Butler <nathanielbutler@hotmail.com>
2024-08-27 12:35:49 +0100

    Add /api/articles endpoint and tests

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { getTopics } = require('./controllers/topics.controllers');
 const { getEndpoints } = require('./controllers/endpoints.controllers');
-const { getArticleById } = require('./controllers/articles.controllers');
+const {
+  getArticles,
+  getArticleById,
+} = require('./controllers/articles.controllers');
 const {
   psqlErrorHandler,
   customErrorHandler,
@@ -12,6 +15,7 @@ const app = express();
 
 app.get('/api', getEndpoints);
 app.get('/api/topics', getTopics);
+app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id', getArticleById);
 
 app.use(psqlErrorHandler);
