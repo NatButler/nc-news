ab37e4930b50646197534a2ea558cbb3fc713888
Nathaniel Butler <nathanielbutler@hotmail.com>
2024-08-28 10:56:40 +0100

    Add sort_by and order query params to /api/articles endpoint plus tests

--- models/articles.models.js
+++ models/articles.models.js
@@ -11,14 +11,35 @@ exports.selectArticleById = (article_id) => {
     });
 };
 
-exports.selectArticles = () => {
-  return db
-    .query(
-      'SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC'
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+exports.selectArticles = (sort_by = 'created_at', order = 'DESC') => {
+  let queryStr =
+    'SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id';
+  const validColumns = [
+    'article_id',
+    'author',
+    'title',
+    'topic',
+    'created_at',
+    'votes',
+    'comment_count',
+  ];
+  const validOrdering = ['ASC', 'DESC'];
+
+  if (!validColumns.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: 'Invalid request' });
+  } else {
+    queryStr += ` ORDER BY ${sort_by}`;
+  }
+
+  if (!validOrdering.includes(order.toUpperCase())) {
+    return Promise.reject({ status: 400, msg: 'Invalid request' });
+  } else {
+    queryStr += ` ${order}`;
+  }
+
+  return db.query(queryStr).then(({ rows }) => {
+    return rows;
+  });
 };
 
 exports.updateArticle = (article_id, reqBody) => {
