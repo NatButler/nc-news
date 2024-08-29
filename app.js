984cf059e1dce818f335d86eb097ec76701506e7
Nathaniel Butler <nathanielbutler@hotmail.com>
2024-08-29 13:30:20 +0100

    Add routers to organise routes

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 const express = require('express');
-const { getTopics } = require('./controllers/topics.controllers');
-const { getEndpoints } = require('./controllers/endpoints.controllers');
-const {
-  getArticles,
-  getArticleById,
-  patchArticle,
-} = require('./controllers/articles.controllers');
-const {
-  getCommentsByArticleId,
-  postComment,
-  deleteComment,
-} = require('./controllers/comments.controllers');
-const { getUsers } = require('./controllers/users.controllers');
+const apiRouter = require('./routes/api-router');
 const {
   psqlErrorHandler,
   customErrorHandler,
@@ -22,15 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/api', getEndpoints);
-app.get('/api/topics', getTopics);
-app.get('/api/articles', getArticles);
-app.get('/api/articles/:article_id', getArticleById);
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-app.post('/api/articles/:article_id/comments', postComment);
-app.patch('/api/articles/:article_id', patchArticle);
-app.delete('/api/comments/:comment_id', deleteComment);
-app.get('/api/users', getUsers);
+app.use('/api', apiRouter);
 
 app.use(psqlErrorHandler);
 app.use(customErrorHandler);
